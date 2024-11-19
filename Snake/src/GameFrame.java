23a68934f77959320bef8fb3d2eaaad76b5a871e
Angel Santiago <155935422+zerodayblitz@users.noreply.github.com>
2024-11-18 21:07:15 -0500

    Add files via upload

---
+++ Snake/src/GameFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+// GameFrame class acts as a empty picture frame
+public class GameFrame extends JFrame {
+    GameFrame() {
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
